fd400320b9bf32aeb6d5fdba8bf2e4873dce3f8c
Temirlan <temirlan.asanov.ta@gmail.com>
2024-06-25 18:26:18 +0500

    mig

---
+++ database/migrations/2024_06_25_192516_change_description_kz_type_in_brand_pages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('brand_pages', function (Blueprint $table) {
+            $table->text('description_kz')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('brand_pages', function (Blueprint $table) {
+            //
+        });
+    }
+};
