4b47b291bb38c37e362f5fcc41818ca0a121351f
Temirlan <temirlan.asanov.ta@gmail.com>
2024-08-07 12:45:39 +0500

    fix

--- app/Http/Requests/Admin/Carera/StoreCareraRequest.php
+++ app/Http/Requests/Admin/Carera/StoreCareraRequest.php
@@ -29,7 +29,7 @@ class StoreCareraRequest extends FormRequest
         return [
             'block1_title' => 'nullable|string',
             'block1_text' => 'nullable|string',
-            'block1_image' => 'nullable|image',
+            'block1_image' => 'nullable',
             'block2_title' => 'nullable|string',
             'block2_text' => 'nullable|string',
             'block2_image' => 'nullable|image',
