c235502879c1290b00ce945030f20c2915192fdd
Temirlan <temirlan.asanov.ta@gmail.com>
2024-10-13 21:46:04 +0500

    fix

--- app/Http/Resources/V1/KomekResource.php
+++ app/Http/Resources/V1/KomekResource.php
@@ -18,9 +18,23 @@ class KomekResource extends JsonResource
         if($lang == 'ru') {
             $title = $this->title;
             $text = $this->text;
+            $card1 = $this->card1;
+            $card2 = $this->card2;
+            $card3 = $this->card3;
+            $card4 = $this->card4;
+            $card5 = $this->card5;
+            $card6 = $this->card6;
+            $services = $this->services;
         } else {
             $title = $this->title_kz;
             $text = $this->text_kz;
+            $card1 = $this->card1_kz;
+            $card2 = $this->card2_kz;
+            $card3 = $this->card3_kz;
+            $card4 = $this->card4_kz;
+            $card5 = $this->card5_kz;
+            $card6 = $this->card6_kz;
+            $services = $this->services_kz;
         }
         return [
             'id' => $this->id,
@@ -28,13 +42,13 @@ class KomekResource extends JsonResource
             'text' => $text,
             'image' => $this->image_url,
             'form_image' => $this->form_image_url,
-            'card1' => $this->card1,
-            'card2' => $this->card2,
-            'card3' => $this->card3,
-            'card4' => $this->card4,
-            'card5' => $this->card5,
-            'card6' => $this->card6,
-            'services' => $this->services
+            'card1' => $card1,
+            'card2' => $card2,
+            'card3' => $card3,
+            'card4' => $card4,
+            'card5' => $card5,
+            'card6' => $card6,
+            'services' => $services,
         ];
     }
 }
