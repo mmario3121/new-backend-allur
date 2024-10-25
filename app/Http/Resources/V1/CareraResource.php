df05f4b1870291f7529a9c73c8a6edf4901c8f5e
Temirlan <temirlan.asanov.ta@gmail.com>
2024-10-25 17:38:52 +0500

    main-page-kz

--- app/Http/Resources/V1/CareraResource.php
+++ app/Http/Resources/V1/CareraResource.php
@@ -32,11 +32,11 @@ class CareraResource extends JsonResource
             $block4_image = $this->block4_image_url;
             $block5_image = $this->block5_image_url;
         }else{
-            $block1_title = $this->block1_title;
-            $block1_text = $this->block1_text;
+            $block1_title = $this->block1_title_kz;
+            $block1_text = $this->block1_text_kz;
             $block1_image = $this->block1_image_url;
-            $block2_title = $this->block2_title;
-            $block2_text = $this->block2_text;
+            $block2_title = $this->block2_title_kz;
+            $block2_text = $this->block2_text_kz;
             $block2_image = $this->block2_image_url;
             $block3_image = $this->block3_image_url;
             $block4_title = $this->block4_title_kz;
