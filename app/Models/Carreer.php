41e6f45075f1348dfb45e1d2d20bc17e5ba42614
Temirlan <temirlan.asanov.ta@gmail.com>
2024-12-23 16:03:36 +0500

    fix

--- app/Models/Carreer.php
+++ app/Models/Carreer.php
@@ -14,8 +14,8 @@ class Carreer extends Model
 
     const CACHE_TIME = 60 * 60 * 24;
     const IMAGE_PATH = 'images/carreers';
-    protected $appends = ['block1_image_url', 'block2_image_url', 'block3_image_url', 'block4_image_url', 'block5_image_url', 'block6_image_url', 'block7_image_url', 'block8_image_url', 'block9_image_url', 'block10_image_url'];
-    protected $fillable = ['block1_title', 'block1_text', 'block1_image', 'block2_title', 'block2_text', 'block2_image', 'block3_title', 'block3_text', 'block3_image', 'block4_title', 'block4_text', 'block4_image', 'block5_title', 'block5_text', 'block5_image', 'block6_title', 'block6_text', 'block6_image', 'block7_title', 'block7_text', 'block7_image', 'block8_title', 'block8_text', 'block8_image', 'block9_title', 'block9_text', 'block9_image', 'block10_title', 'block10_text', 'block10_image', 'card1_title', 'card1_text', 'card2_title', 'card2_text', 'card3_title', 'card3_text', 'card4_title', 'card4_text'];
+    protected $appends = ['block1_image_url', 'block2_image_url', 'block3_image_url', 'block4_image_url', 'block5_image_url', 'block6_image_url', 'block7_image_url', 'block8_image_url', 'block9_image_url', 'block10_image_url', 'block11_image_url'];
+    protected $fillable = ['block1_title', 'block1_text', 'block1_image', 'block2_title', 'block2_text', 'block2_image', 'block3_title', 'block3_text', 'block3_image', 'block4_title', 'block4_text', 'block4_image', 'block5_title', 'block5_text', 'block5_image', 'block6_title', 'block6_text', 'block6_image', 'block7_title', 'block7_text', 'block7_image', 'block8_title', 'block8_text', 'block8_image', 'block9_title', 'block9_text', 'block9_image', 'block10_title', 'block10_text', 'block10_image', 'block11_title', 'block11_text', 'block11_image', 'card1_title', 'card1_text', 'card2_title', 'card2_text', 'card3_title', 'card3_text', 'card4_title', 'card4_text'];
 
     protected $hidden = ['updated_at', 'created_at'];
 
@@ -69,5 +69,10 @@ class Carreer extends Model
     public function getBlock10ImageUrlAttribute(): string|null
     {
         return $this->block10_image ? Storage::disk('custom')->url(self::IMAGE_PATH . '/' . $this->block10_image) : null;
+    }
+
+    public function getBlock11ImageUrlAttribute(): string|null
+    {
+        return $this->block11_image ? Storage::disk('custom')->url(self::IMAGE_PATH . '/' . $this->block11_image) : null;
     }
 }
